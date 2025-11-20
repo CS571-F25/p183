2420903f3b0960a270845909becf38a6f2d39d7f
Shivani <shivanipotnuru@gmail.com>
2025-11-19 23:29:04 -0600

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-
-import NavBar from './components/NavBar.jsx'
-import Home from './components/Home.jsx'
-import AboutMe from './components/AboutMe.jsx'
-import Resume from './components/Work.jsx'
-import Contact from './components/Contact.jsx'
-
-import './App.css'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
+import Home from "./components/Home.jsx";
+import AboutMe from "./components/AboutMe.jsx";
+import Work from "./components/Work.jsx";
+import Contact from "./components/Contact.jsx";
+import "./App.css";
 
 function App() {
   return (
     <HashRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<AboutMe />}/>
-        <Route path="/resume" element={<Resume />}/>
-        <Route path="/contact" element={<Contact />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </HashRouter>
   );
